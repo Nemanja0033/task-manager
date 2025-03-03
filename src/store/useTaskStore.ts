5129c70b3562162de15fbcc4840c5d3aace3e099
nemanja <antonijevicnemanja68@gmail.com>
2025-03-03 14:35:26 +0100

    settting task state in store

--- src/store/useTaskStore.ts
+++ src/store/useTaskStore.ts
@@ -1,3 +1,20 @@
 import { create } from "zustand";
+import { TaskStore } from "../types/TaskStoreTypes";
 
-
+export const useTaskStore = create<TaskStore>((set) => ({
+    tasks: [],
+    addTask: (title) => //adding task to state
+      set((state) => ({
+        tasks: [...state.tasks, { id: crypto.randomUUID(), title, completed: false }],
+      })),
+    toggleTask: (id: string) => //toggling task from state
+      set((state) => ({
+        tasks: state.tasks.map((task) =>
+          task.id === id ? { ...task, completed: !task.completed } : task
+        ),
+      })),
+    removeTask: (id) => // removing task from state
+      set((state) => ({
+        tasks: state.tasks.filter((task) => task.id !== id),
+      })),
+  }));
